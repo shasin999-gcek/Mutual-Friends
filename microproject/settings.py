d5de0bc7962afb22dd52cf2e326f97b64d8fc633
Muhammed Shasin P <mohdshasin313@gmail.com>
2019-03-26 22:23:07 +0530

    Static root path fix

--- microproject/settings.py
+++ microproject/settings.py
@@ -119,6 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'mutualfriends/static')
